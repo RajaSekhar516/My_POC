10776fae0bb8bd374f676c353e0ed36d826a7987
RajaSekhar516 <95454579+RajaSekhar516@users.noreply.github.com>
2023-10-11 14:07:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,37 @@ provider "aws" {
   secret_key = "FQTWV1aF2B/ZrmXV1FgwCZQf/9edaCKwnu+b96qB"
   alias = "usa"
 }
+#Create security group with firewall rules
+resource "aws_security_group" "jenkins-sg" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
 # create EC2 instances in multiple regions
 resource "aws_instance" "Verginaia-aserver" {
   ami           = "ami-067d1e60475437da2"
