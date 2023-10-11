6c6d24e4884db4694f5014a63fa4cb01338c3a02
RajaSekhar516 <95454579+RajaSekhar516@users.noreply.github.com>
2023-10-11 13:16:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,76 +1,30 @@
 provider "aws" {
-  region = var.aws_region
+  region      = "us-east-1"
+  access_key  = "AKIAY5HZIULKXVW6RVXE"
+  secreat_key = "FQTWV1aF2B/ZrmXV1FgwCZQf/9edaCKwnu+b96qB"
 }
 provider "aws" {
-  region = var.aws_region2
-}
-resource "aws_vpc" "main" {
-  cidr_block = "172.31.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "main"
-  }
-}
-
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
+  region =  "us-east-2"
+  access_key  = "AKIAY5HZIULKXVW6RVXE"
+  secreat_key = "FQTWV1aF2B/ZrmXV1FgwCZQf/9edaCKwnu+b96qB"
+  alias = "usa"
 }
 # create EC2 instances in multiple regions
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
-  provider = aws.us.east-1 # use the alias for the region
+resource "aws_instance" "Verginaia-aserver" {
+  ami           = "ami-067d1e60475437da2"
+  key_name = "raja"
+  instance_type = "t2.micro"
   tags= {
-    Name = var.tag_name
+    Name = "Verginaia-aserver"
   }
 }
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id1
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
-  provider = aws.us.east-2 # use the alias for the region
+resource "aws_instance" "ohio-server" {
+  ami           = "ami-036f5574583e16426"
+  key_name = "raja"
+  instance_type = "t2.micro"
+  provider     = aws.usa
   tags= {
-    Name = var.tag_name
+    Name = "ohio-server"
   }
 }
 
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
